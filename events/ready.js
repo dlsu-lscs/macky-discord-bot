4efe6eb8608d09966cf5c09bf307fecbdfc4ba38
Sean Denzel Y. Robenta (zel) <38722112+zelkim@users.noreply.github.com>
2025-03-26 21:52:51 +0800

    Revert "Reddit"

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
-const check_reddit = require("../controllers/check_reddit");
 
 module.exports = {
     name: "ready",
     once: true,
     execute(bot) {
         //Log Bot's username and the amount of servers its in to console
-        console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
+        console.log(
+            `${bot.user.username} is online on ${bot.guilds.cache.size} servers!`
+        );
 
         //Set the Presence of the bot user
         bot.user.setPresence({ activities: [{ name: "Animo, La Salle!" }] });
@@ -23,16 +24,23 @@ module.exports = {
         });
 
         // Verify
-        app.get("/verify/:id", passBotInstance, require("../controllers/verify"));
+        app.get(
+            "/verify/:id",
+            passBotInstance,
+            require("../controllers/verify")
+        );
 
         // Facebook webhooks
-        app.get("/webhooks", passBotInstance, require("../controllers/webhook_verification"));
-        app.post("/webhooks", passBotInstance, require("../controllers/webhook_notification"));
-
-        check_reddit(bot);
-        setInterval(() => {
-            check_reddit(bot).catch(console.error);
-        }, 60000 * process.env.REDDIT_INTERVAL_MINS);
+        app.get(
+            "/webhooks",
+            passBotInstance,
+            require("../controllers/webhook_verification")
+        );
+        app.post(
+            "/webhooks",
+            passBotInstance,
+            require("../controllers/webhook_notification")
+        );
     },
 };
 
